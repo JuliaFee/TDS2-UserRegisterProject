a79b85c0b48e7520fce7f4c9bcff2c098528c5dc
JuliaFee <julia.silva133@aluno.senai.br>
2023-08-22 10:21:24 -0300

    feat: create js function showUsers

--- assets/js/script.index.js
+++ assets/js/script.index.js
@@ -2,11 +2,46 @@ class User{
     constructor(name, email, birthdate, city, phone, cpf, status){
         this.name = name;
         this.email = email;
-        this.birthdate = birthdate;
+        this.birthdate = birthdate.split('-').reverse().join('/');
         this.city = city;
         this.phone = phone;
         this.cpf = cpf;
         this.status = status;
+        this.sign = this.getZodiacSign();
+    }
+
+    
+    getZodiacSign() {
+        let birthdate = new Date(this.birthdate);
+        let day = birthdate.getDate();
+        let month = birthdate.getMonth() + 1;
+        console.log("Passou pelo getSigno() da class User");
+    
+        if ((month == 1 && day <= 20) || (month == 12 && day >= 22)) {
+            this.sign = "Capricórnio ♑";
+        } else if ((month == 1 && day >= 21) || (month == 2 && day <= 18)) {
+            this.sign = "Aquário ♒";
+        } else if ((month == 2 && day >= 19) || (month == 3 && day <= 20)) {
+            this.sign = "Peixes ♓";
+        } else if ((month == 3 && day >= 21) || (month == 4 && day <= 20)) {
+            this.sign = "Áries ♈";
+        } else if ((month == 4 && day >= 21) || (month == 5 && day <= 20)) {
+            this.sign = "Touro ♉";
+        } else if ((month == 5 && day >= 21) || (month == 6 && day <= 20)) {
+            this.sign = "Gêmeos ♊";
+        } else if ((month == 6 && day >= 22) || (month == 7 && day <= 22)) {
+            this.sign = "Câncer ♋";
+        } else if ((month == 7 && day >= 23) || (month == 8 && day <= 23)) {
+            this.sign = "Leão ♌";
+        } else if ((month == 8 && day >= 24) || (month == 9 && day <= 23)) {
+            this.sign = "Virgem ♍";
+        } else if ((month == 9 && day >= 24) || (month == 10 && day <= 23)) {
+            this.sign = "Libra ♎";
+        } else if ((month == 10 && day >= 24) || (month == 11 && day <= 22)) {
+            this.sign = "Escorpião ♏";
+        } else if ((month == 11 && day >= 23) || (month == 12 && day <= 21)) {
+            this.sign = "Sagitário ♐";
+        }
     }
 }
 
@@ -16,47 +51,14 @@ class ListUser{
     }
     addUser(user){
         this.users.push(user);
-
     }
-    getZodiacSign() {
-        let birthdate = new Date(this.birthdate);
-        let day = birthdate.getDate();
-        let month = birthdate.getMonth() + 1;
-        console.log("Passou pelo getSigno() da class User");
     
-        if ((month == 1 && day <= 20) || (month == 12 && day >= 22)) {
-            return "Capricórnio ♑";
-        } else if ((month == 1 && day >= 21) || (month == 2 && day <= 18)) {
-            return "Aquário ♒";
-        } else if ((month == 2 && day >= 19) || (month == 3 && day <= 20)) {
-            return "Peixes ♓";
-        } else if ((month == 3 && day >= 21) || (month == 4 && day <= 20)) {
-            return "Áries ♈";
-        } else if ((month == 4 && day >= 21) || (month == 5 && day <= 20)) {
-            return "Touro ♉";
-        } else if ((month == 5 && day >= 21) || (month == 6 && day <= 20)) {
-            return "Gêmeos ♊";
-        } else if ((month == 6 && day >= 22) || (month == 7 && day <= 22)) {
-            return "Câncer ♋";
-        } else if ((month == 7 && day >= 23) || (month == 8 && day <= 23)) {
-            return "Leão ♌";
-        } else if ((month == 8 && day >= 24) || (month == 9 && day <= 23)) {
-            return "Virgem ♍";
-        } else if ((month == 9 && day >= 24) || (month == 10 && day <= 23)) {
-            return "Libra ♎";
-        } else if ((month == 10 && day >= 24) || (month == 11 && day <= 22)) {
-            return "Escorpião ♏";
-        } else if ((month == 11 && day >= 23) || (month == 12 && day <= 21)) {
-            return "Sagitário ♐";
-        }
-    }
 }
 
 const listUser = new ListUser();
 
 function createUser(){
-    console.log("entrou createUser")
-    //
+    console.log("entrou createUser");
    const name = document.getElementById("name").value;
    const email = document.getElementById("email").value;
    const birthdate = document.getElementById("birthdate").value;
@@ -64,10 +66,6 @@ function createUser(){
    const phone = document.getElementById("phone").value;
    const cpf = document.getElementById("cpf").value;
    const status = false; /* vai retornar false para reprensentar nao registrado */
-//
-   console.log(`hello ${name}, your email is ${email}, your birthdate is ${birthdate},
-   you live in ${city}, your phone number is ${phone}, your cpf is ${cpf}`)
-   //
    const user = new User (name, email, birthdate, city, phone, cpf, status);
    listUser.addUser(user);
 
@@ -81,7 +79,13 @@ function showRegister() {
     document.getElementById("title-page").classList.remove("hidden");
     document.getElementById("main-div").classList.remove("hidden");
     console.log("Passou pela funcao showRegister()");
+}
 
+function showUsers(){
+    console.log("entrou pela funcao showUsers");
+    document.getElementById("main-div").classList.add("hidden");
+    document.getElementById("title-page").classList.add("hidden");
+    document.getElementById("sub-div").classList.remove("hidden");
 }
 
 function formatedCPF(cpf) {
